34f762c6fc6f370bddadf18a64d34e93cebc5b28
chandrasekhar.galla <chandrasekhar.galla@E1800F1D.lsz.ads.valuelabs.net>
2017-07-24 17:38:14 +0530

    test commit

--- testProj/src/Test.java
+++ testProj/src/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("vinayaka ayyapaa");
+		System.out.println("test git");
 		
 		System.out.println("test");
 
